d2a2a54f0253c3560b1db6881c4dffb4ae104834
merrickt <39343323+merrickt@users.noreply.github.com>
2020-07-09 17:48:40 -0400

    Updates

---
+++ precache-manifest.6ccbcbb3ffac21d8b465e1c51e79ada9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e44554d49646a95ed69ba784611cf2f",
+    "url": "/Cake-Animations/index.html"
+  },
+  {
+    "revision": "a67ebac540a8fadb61f2",
+    "url": "/Cake-Animations/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7bcac8fe97c5be05909d",
+    "url": "/Cake-Animations/static/js/2.7451768d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cake-Animations/static/js/2.7451768d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67ebac540a8fadb61f2",
+    "url": "/Cake-Animations/static/js/main.884f1467.chunk.js"
+  },
+  {
+    "revision": "a2c07db6dc147fd8469e",
+    "url": "/Cake-Animations/static/js/runtime-main.54dd2b87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cake-Animations/static/media/logo.5d5d9eef.svg"
+  }
+]);
