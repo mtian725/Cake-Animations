67a7d5866294a7f114f8c9d12321a3f256c5b18e
merrickt <39343323+merrickt@users.noreply.github.com>
2020-07-12 19:05:10 -0400

    Updates

--- precache-manifest.0f57350ff67f5e3211c1d541823fb122.js
+++ precache-manifest.0f57350ff67f5e3211c1d541823fb122.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "26137f766584718d064c4e342cede31d",
+    "revision": "f78cb5ed27c3e77660b5be170c491a34",
     "url": "/Cake-Animations/index.html"
   },
   {
-    "revision": "ed56cd8d115c99aedf24",
+    "revision": "29476139125ffe1e39c9",
     "url": "/Cake-Animations/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "489c3eaf277ab83366be",
+    "revision": "2073baffbf1f2e9acc4b",
     "url": "/Cake-Animations/static/css/main.7b9d915c.chunk.css"
   },
   {
-    "revision": "ed56cd8d115c99aedf24",
-    "url": "/Cake-Animations/static/js/2.63110b11.chunk.js"
+    "revision": "29476139125ffe1e39c9",
+    "url": "/Cake-Animations/static/js/2.5d1e3adb.chunk.js"
   },
   {
     "revision": "0e539e7825f064abfe260a524bc0ab3b",
-    "url": "/Cake-Animations/static/js/2.63110b11.chunk.js.LICENSE.txt"
+    "url": "/Cake-Animations/static/js/2.5d1e3adb.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "489c3eaf277ab83366be",
-    "url": "/Cake-Animations/static/js/main.d2e2d2d4.chunk.js"
+    "revision": "2073baffbf1f2e9acc4b",
+    "url": "/Cake-Animations/static/js/main.cc6e9519.chunk.js"
   },
   {
     "revision": "a2c07db6dc147fd8469e",
